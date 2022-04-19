2b72843325ca67a5449d8c836c0148288ab871df
Maksim Zemlyanikin <zemlyanikin.maksim@gmail.com>
2022-04-19 23:09:50 +0400

    Format `isolate_messenger.dart`

--- lib/src/isolate_messenger/isolate_messenger.dart
+++ lib/src/isolate_messenger/isolate_messenger.dart
@@ -1,6 +1,5 @@
-
-/// [IsolateMessenger] is used to chat with `Isolate`. 
-/// It has [messages] getter with stream of messages from `Isolate` 
+/// [IsolateMessenger] is used to chat with `Isolate`.
+/// It has [messages] getter with stream of messages from `Isolate`
 /// and [send] method which sends message to `Isolate`.
 abstract class IsolateMessenger {
   const IsolateMessenger();
